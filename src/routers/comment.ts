c97ed630dbcda5e429f887f18322b3e08ea278c8
Song Yoon <ysong0504@gmail.com>
2021-07-26 16:36:13 +0900

    [Song]<feat> Add 'getTaggedUser' function and 'like' API. Apply 'authMiddleware' to apis that require authentication

--- src/routers/comment.ts
+++ src/routers/comment.ts
@@ -1,147 +1,149 @@
-import express, { Request, Response, NextFunction, Router, response } from "express";
+import express, {Request,Response,NextFunction,Router,response,} from "express";
 import Comments from "../schemas/comment";
-import moment from 'moment';
+import { authMiddleware } from "./auth-middleware";
+import moment from "moment";
 
 const commentRouter = express.Router();
 
-// 댓글 업로드 auth middleware 추가 필요
-commentRouter.post("/:drinkId", async(req, res) => {
-    try {
-        const { drinkId } = req.params;
-        const { content } = req.body;
-        if (!content) {
-            res.status(400).send({
-                message: "내용이 없습니다."
-            });
-            return;
-        }
-        // local stroage 확인 필요
-        // const userId = res.locals.user.userId;
-        const userId = '12345'
-        // date 확인 필요 (moment 라이브러리 사용)
-        const date = moment().format('YYYY-MM-DD');
-    
-        await Comments.create({ drinkId, userId, content, date });
-    
-        res.json({ message: 'success' });
+// 댓글 업로드
+commentRouter.post("/:drinkId", authMiddleware, async (req, res) => {
+  try {
+    const { drinkId } = req.params;
+    const { content } = req.body;
+    if (!content) {
+      res.status(400).send({
+        message: "내용이 없습니다.",
+      });
+      return;
+    }
+    const userId = res.locals.user._id;
+    const date = moment().format("YYYY-MM-DD hh:mm A"); // 날짜 포맷 변경
+    const taggedUser = getTaggedUser(content); // @ 기준 태그된 유저 가져오기
 
-    } catch(err) {
-        res.status(400).send({
-            message: `[댓글 업로드] ${err}`
-        });
-    };
+    await Comments.create({
+      drinkId,
+      userId,
+      content,
+      date,
+      tagged_array: taggedUser,
+    });
+    res.json({ message: "success" });
+  } catch (err) {
+    res.status(400).send({
+      message: `[댓글 업로드] ${err}`,
+    });
+  }
 });
 
+// 댓글 조회
+commentRouter.get("/:drinkId", async (req, res) => {
+  try {
+    const { drinkId } = req.params;
+    if (!drinkId) {
+      // drinkId가 없을 시 진행 X
+      res.status(400).send({
+        message: "drinkId 가 없습니다.",
+      });
+      return;
+    }
+    const comment = await Comments.find({ drinkId }).sort("-date").lean();
 
-// 주류 별 댓글 조회 
-commentRouter.get('/:drinkId', async (req, res) => {
-    try {     
-        const { drinkId } = req.params;
-        // drinkId가 없을 시 진행 X
-        if (!drinkId) {
-            res.status(400).send({
-                message: "drinkId 가 없습니다."
-            });
-            return;
-        };
-
-        const comment = await Comments.findOne({ drinkId });
-        res.json({ result: comment })
-            
-    } catch (err) {
-        res.status(400).send({
-            message: `[댓글 조회] ${err}`
-        });
-    };
+    res.json({ result: comment });
+  } catch (err) {
+    res.status(400).send({
+      message: `[댓글 조회] ${err}`,
+    });
+  }
 });
 
-// 댓글 삭제 auth middleware 추가 필요
-commentRouter.delete('/:commentId', async (req, res) => {
-    try {     
-        const { commentId } = req.params;
-        // drinkId가 없을 시 진행 X
-        if (!commentId) {
-            res.status(400).send({
-                message: "commentId 가 없습니다."
-            })
-            return;
-        }
-        await Comments.deleteOne({ _id: commentId });
-        res.json({ message: 'success' });
-            
-    } catch (err) {
-        res.status(400).send({
-            message: `[댓글 삭제] ${err}`
-        });
-    };
+// 댓글 삭제
+commentRouter.delete("/:commentId", authMiddleware, async (req, res) => {
+  try {
+    const { commentId } = req.params;
+    if (!commentId) {
+      // drinkId가 없을 시 진행 X
+      res.status(400).send({
+        message: "commentId 가 없습니다.",
+      });
+      return;
+    }
+    await Comments.deleteOne({ _id: commentId });
+    res.json({ message: "success" });
+  } catch (err) {
+    res.status(400).send({
+      message: `[댓글 삭제] ${err}`,
+    });
+  }
 });
 
+// 댓글 수정
+commentRouter.put("/:commentId", authMiddleware, async (req, res) => {
+  try {
+    const { commentId } = req.params;
+    const { content } = req.body;
+    if (!commentId || !content) {
+      // drinkId 또는 content가 없을 시 진행 X
+      res.status(400).send({
+        message: "commentId 또는 content를 확인해주세요.",
+      });
+      return;
+    }
+    const taggedUser = getTaggedUser(content); // @ 기준 태그된 유저 가져오기
 
-// 댓글 수정 auth middleware 추가 필요
-commentRouter.put('/:commentId', async (req, res) => {
-    try {     
-        const { commentId } = req.params;
-        const { content } = req.body;
-        // drinkId 또는 수정된 내용이 없을 시 진행 X
-        if (!commentId || !content) {
-            res.status(400).send({
-                message: "commentId 또는 content를 확인해주세요."
-            });
-            return;
-        };
+    await Comments.updateOne(
+      { _id: commentId },
+      { $set: { content: content, edited: true, tagged_array: taggedUser } } // 내용 수정 시 'edited'는 true값으로 변경.
+    );
 
-        // 댓글 내용 수정 시 'edited'는 true값으로 변경한다. 
-        await Comments.updateOne({ _id: commentId }, { $set: { content: content, edited: true } });
-
-        res.json({ message: 'success' });
-            
-    } catch (err) {
-        res.status(400).send({
-            message: `[댓글 수정] ${err}`
-        });
-    };
+    res.json({ message: "success" });
+  } catch (err) {
+    res.status(400).send({
+      message: `[댓글 수정] ${err}`,
+    });
+  }
 });
 
+// 댓글 좋아요
+commentRouter.put("/like/:commentId", authMiddleware, async (req, res) => {
+  try {
+    const { commentId } = req.params;
+    const comment = await Comments.findOne({ _id: commentId });
+    if (!comment) {
+      res.status(400).send({
+        message: "commentId 를 확인해주세요.",
+      });
+      return;
+    }
 
-// 댓글 좋아요 auth middleware 추가 필요
-commentRouter.post('/:commentId', async (req, res) => {
-    try {     
-        const { commentId } = req.params;
-        // comment 작성자 가져오기
-        const commentWriter = await Comments.findOne({ _id: commentId })
+    const userId = res.locals.user._id;
+    if (comment.like_array.includes(userId)) {
+      // '좋아요'를 누른 내역이 있는지 확인
+      await Comments.updateOne(
+        { _id: commentId },
+        { $pull: { like_array: userId } } // 있으면 배열에서 제거
+      );
+    } else {
+      await Comments.updateOne(
+        { _id: commentId },
+        { $push: { like_array: userId } } // 없으면 배열에 추가
+      );
+    }
 
-        if (!commentWriter) {
-            res.status(400).send({
-                message: "commentId 를 확인해주세요."
-            });
-            return;
-        }
-
-        // local stroage 확인 필요
-        // const userId = res.locals.user.userId;
-        const userId = '12345'
-        // 댓글 내용 수정 시 'edited'는 true값으로 변경한다. 
-
-        // '좋아요'를 누른 내역이 있는지 확인
-
-        // 있으면 배열에서 제거
-
-        // 없으면 배열에 추가
-
-
-        await Comments.updateOne(
-            { _id: commentId }, 
-            { $push: { like_array: { userId } } }
-        );
-
-        res.json({ message: 'success' });
-            
-    } catch (err) {
-        res.status(400).send({
-            message: `[댓글 수정] ${err}`
-        });
-    };
+    res.json({ message: "success" });
+  } catch (err) {
+    res.status(400).send({
+      message: `[댓글 좋아요] ${err}`,
+    });
+  }
 });
 
+// @ 기준 태그된 유저 가져온 후 @ 제거된 순수 닉네임만 저장
+function getTaggedUser(content: any) {
+  let taggedUser = content.match(/@[a-z가-힣1-9]+/gi);
+  for (let i in taggedUser) {
+    taggedUser[i] = taggedUser[i].replace("@", "");
+  }
+  return taggedUser;
+}
 
 export { commentRouter };
