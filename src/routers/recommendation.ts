9f342d99e034c6001cdf894fc0c13667eb0d97bf
Garden <prepella3@gmail.com>
2021-07-30 09:39:36 +0900

    [garden] <feat> add slack token

--- src/routers/recommendation.ts
+++ src/routers/recommendation.ts
@@ -3,7 +3,7 @@ import webhook, { WebClient, LogLevel } from "@slack/web-api";
 
 import { authMiddleware } from "../middlewares/auth-middleware";
 
-const client = new WebClient("xoxb-2304718692502-2319759042067-K8cXXoOwrSSCkoLj8FDu3fqx", {
+const client = new WebClient("xoxb-2304718692502-2319759042067-vKLTaKEX7ilriuklZDpYDYEP", {
     logLevel: LogLevel.DEBUG
 });
 
