0ba2c5623bade33cee5df8f895169ff81d7c4ef1
Garden <62330320+gar-den@users.noreply.github.com>
2021-07-30 10:06:58 +0900

    Update recommendation.ts

--- src/routers/recommendation.ts
+++ src/routers/recommendation.ts
@@ -3,7 +3,7 @@ import webhook, { WebClient, LogLevel } from "@slack/web-api";
 
 import { authMiddleware } from "../middlewares/auth-middleware";
 
-const client = new WebClient("xoxb-2304718692502-2319759042067-KsrLQfnbqXjC9iOuK2IIlWy2", {
+const client = new WebClient("xoxb-2304718692502-2319759042067-mVygWAcYJVHqEOwZGb9bCJKj", {
     logLevel: LogLevel.DEBUG
 });
 
