b852fb3c2e44a6746484160d24f829b18fc75aff
moonjinyoung <wlsdud131@gmail.com>
2021-08-02 15:08:42 +0900

    updated env variables

--- src/env.ts
+++ src/env.ts
@@ -7,6 +7,12 @@ export const env = {
     isProduction: process.env.NODE_ENV === "production",
     isTest: process.env.NODE_ENV === "test",
     port: process.env.PORT,
-    url: String(process.env.URL)
-
+    url: String(process.env.URL),
+    clientId_kakao: String(process.env.CLIENTID_KAKAO),
+    clientSecret_kakao: String(process.env.CLIENTSECRET_KAKAO),
+    callbackUrl_kakao: String(process.env.CALLBACKURL_KAKAO),
+    clientId_google: String(process.env.CLIENTID_GOOGLE),
+    clientSecret_google: String(process.env.CLIENTSECRET_GOOGLE),
+    callbackUrl_google: String(process.env.CALLBACKURL_GOOGLE),
+    botUserOAuthToken: String(process.env.BOTUSEROAUTHTOKEN),
 };
