cb5a6d032f34e2ec3f6a01b260956e7b3e293772
Song Yoon <ysong0504@gmail.com>
2021-08-04 16:27:39 +0900

    [Song]<cd>test

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -6,10 +6,6 @@ sudo chmod -R 777 /home/ec2-user/ohsool
 # EC2 작업 파일로 이동
 cd /home/ec2-user/ohsool
 
-# production 상태로 전환
-export NODE_ENV=production
-echo "${NODE_ENV} mode is on"
-
 # npm과 node 설치
 export NVM_DIR="$HOME/.nvm"	
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm	
@@ -25,6 +21,9 @@ npm install
 # typescript를 javascript 로 complie 하기
 npm run build
 
+# production 상태로 전환
+export NODE_ENV=production
+echo "${NODE_ENV} mode is on"
 
 # node 어플리케이션 background에서 실행시키기 (by doing so, the server won't be terminated due to inactivates)
 # node app.js 만 입력시 foreground로 실행이 됌
