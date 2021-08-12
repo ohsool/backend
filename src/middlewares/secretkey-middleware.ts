a2ef8b58e389fbd4a25e08259b3c37972a2e6a03
Garden <prepella3@gmail.com>
2021-08-12 16:14:08 +0900

    [garden] <feat> secret key given to header

---
+++ src/middlewares/secretkey-middleware.ts
@@ -0,0 +1,23 @@
+import express, { Application, Request, Response, NextFunction } from "express";
+import { secretAPIkey } from "../ssl/secretAPI";
+
+function secretKeyMiddleware(req: Request, res: Response, next: NextFunction){
+    const received_key = req.headers.secretkey;
+
+    try {
+        const secretkey = secretAPIkey();
+        
+        if (received_key == secretkey) {
+            next();
+        } else {
+            res.json({ message: "fail", error: "wrong key"});
+
+            return;
+        }
+
+    } catch (error) {
+        res.json({ message: "fail", error});
+    }
+}
+
+export { secretKeyMiddleware };
